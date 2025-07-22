35636a40233b9bcfb17a1e9183b9f0329256ff0c
c0der4t <mekron@ekronds.co.za>
2025-07-22 11:50:03 +0200

    Add linked list implementation with Node class and count method

---
+++ prjLinkList/Node.cs
@@ -0,0 +1,13 @@
+namespace prjLinkList
+{
+    class Node
+    {
+        public int value;
+        public Node next;
+
+        public Node(int value)
+        {
+            this.value = value;
+        }
+    }
+}
---
+++ prjLinkList/Program.cs
@@ -0,0 +1,22 @@
+﻿Node n = new Node(6);
+Node na = new Node(7);
+Node nb = new Node(9);
+
+n.next = na;
+na.next = nb;
+
+System.Console.WriteLine(countFromGivenNode(n));
+
+
+static int countFromGivenNode (Node Head) {
+
+    int count = 0;
+    Node current = Head;
+
+    while (current.next != null) {
+        count++;
+        current = current.next;
+    }
+
+    return count;
+}
